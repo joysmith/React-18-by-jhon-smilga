498ae4c1fa8b67fdd9a8cd7f4f4a46c59f47cdbf
Joy <joysmith271@gmail.com>
2023-08-30 18:16:48 +0700

    vite notes

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
+import "./index.css";
 
-import { books } from './books';
-import Book from './Book';
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
     <>
       <h1>amazon best sellers</h1>
-      <section className='booklist'>
+      <section className="booklist">
         {books.map((book) => {
           return <Book {...book} key={book.id} />;
         })}
@@ -19,6 +19,6 @@ function BookList() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
